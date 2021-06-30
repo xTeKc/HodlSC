909c6e282663d633ca3fb996de9692a085f5a145
xTeKc <bcd3v@pm.me>
2021-06-30 10:27:50 -0400

    update config

--- truffle-config.js
+++ truffle-config.js
@@ -78,17 +78,19 @@ module.exports = {
   mocha: {
     // timeout: 100000
   },
+  contracts_directory: './src/contracts/',
+  contracts_build_directory: './src/abis/',
 
   // Configure your compilers
   compilers: {
     solc: {
        version: "0.8.0",    // Fetch exact version from solc-bin (default: truffle's version)
       // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
-      // settings: {          // See the solidity docs for advice about optimization and evmVersion
-      //  optimizer: {
-      //    enabled: false,
-      //    runs: 200
-      //  },
+       settings: {          // See the solidity docs for advice about optimization and evmVersion
+        optimizer: {
+          enabled: false,
+          runs: 200
+        },
       //  evmVersion: "byzantium"
       // }
     }
